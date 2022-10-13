538ac20f71475ca91fa167c7214542bc4d555e85
Dario Sechi <dario.sechi.rnd@gmail.com>
2022-10-13 23:13:57 +0200

    Remove environment options from vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,11 +6,6 @@ export default defineConfig({
       exclude: ['src/index.ts']
     },
     environment: 'jsdom',
-    environmentOptions: {
-      jsdom: {
-        resources: 'usable'
-      }
-    },
     include: ['tests/**/*.test.ts']
   }
 })
