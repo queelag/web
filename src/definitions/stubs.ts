7f87a6ca473be64ded4dce05e9c62fba11a4f1a6
Dario Sechi <dario.sechi.rnd@gmail.com>
2022-10-27 00:25:38 +0200

    Make IsomorphicEvent extend Event instead of CustomEvent

--- src/definitions/stubs.ts
+++ src/definitions/stubs.ts
@@ -1,13 +1,12 @@
 import { STUB_COOKIE_GET, STUB_COOKIE_SET, STUB_STORAGE } from '@queelag/core'
 
-export class StubCustomEvent<T> implements CustomEvent<T> {
+export class StubEvent implements Event {
   readonly bubbles: boolean
   cancelBubble: boolean
   readonly cancelable: boolean
   readonly composed: boolean
   readonly currentTarget: EventTarget | null
   readonly defaultPrevented: boolean
-  readonly detail: T
   readonly eventPhase: number
   readonly isTrusted: boolean
   /** @deprecated */
@@ -18,12 +17,7 @@ export class StubCustomEvent<T> implements CustomEvent<T> {
   readonly timeStamp: DOMHighResTimeStamp
   readonly type: string
 
-  readonly AT_TARGET: number
-  readonly BUBBLING_PHASE: number
-  readonly CAPTURING_PHASE: number
-  readonly NONE: number
-
-  constructor(type: string, eventInitDict?: CustomEventInit<T>) {
+  constructor(type: string, eventInitDict?: EventInit) {
     this.AT_TARGET = 2
     this.BUBBLING_PHASE = 3
     this.CAPTURING_PHASE = 1
@@ -35,7 +29,6 @@ export class StubCustomEvent<T> implements CustomEvent<T> {
     this.composed = eventInitDict?.composed ?? false
     this.currentTarget = null
     this.defaultPrevented = false
-    this.detail = eventInitDict?.detail as T
     this.eventPhase = this.NONE
     this.isTrusted = false
     this.returnValue = false
@@ -49,11 +42,16 @@ export class StubCustomEvent<T> implements CustomEvent<T> {
     return []
   }
 
+  /** @deprecated */
   initEvent(type: string, bubbles?: boolean, cancelable?: boolean): void {}
-  initCustomEvent(type: string, bubbles?: boolean, cancelable?: boolean, detail?: T): void {}
   preventDefault(): void {}
   stopImmediatePropagation(): void {}
   stopPropagation(): void {}
+
+  readonly AT_TARGET: number
+  readonly BUBBLING_PHASE: number
+  readonly CAPTURING_PHASE: number
+  readonly NONE: number
 }
 
 export const STUB_DOCUMENT_COOKIE_MAP: Map<string, string> = new Map()
--- src/events/isomorphic.event.ts
+++ src/events/isomorphic.event.ts
@@ -1,7 +1,19 @@
-import { StubCustomEvent } from '../definitions/stubs'
+import { StubEvent } from '../definitions/stubs'
 
-if (typeof CustomEvent !== 'function') {
-  global.CustomEvent = StubCustomEvent
+if (typeof Event !== 'function') {
+  global.Event = StubEvent as typeof Event
 }
 
-export class IsomorphicEvent<T> extends CustomEvent<T> {}
+interface IsomorphicEventInit<T = any> extends EventInit {
+  detail?: T
+}
+
+export class IsomorphicEvent<T = any> extends Event {
+  readonly detail?: T
+
+  constructor(type: string, eventInitDict?: IsomorphicEventInit<T>) {
+    super(type, eventInitDict)
+
+    this.detail = eventInitDict?.detail
+  }
+}
