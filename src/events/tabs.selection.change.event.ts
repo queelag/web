993c2cdc218a6e21804670dad4c78216df6b6299
Dario Sechi <dario.sechi.rnd@gmail.com>
2022-11-05 23:25:02 +0100

    Add tabs selection change event

---
+++ src/events/tabs.selection.change.event.ts
@@ -0,0 +1,12 @@
+import { IsomorphicEvent } from './isomorphic.event'
+
+interface Detail<T extends HTMLElement> {
+  selectedTab?: T
+  selectedTabIndex: number
+}
+
+export class TabsSelectionChangeEvent<T extends HTMLElement> extends IsomorphicEvent<Detail<T>> {
+  constructor(selectedTab: T | undefined, selectedTabIndex: number) {
+    super('selection-change', { detail: { selectedTab, selectedTabIndex } })
+  }
+}
