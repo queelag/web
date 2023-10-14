252e5763b9a99e5efa912e04f3775c196e0117e1
Dario Sechi <dario.sechi.rnd@gmail.com>
2023-10-14 21:04:32 +0200

    Use undefined instead of null for the StateChangeEvent

--- src/events/state-change-event.ts
+++ src/events/state-change-event.ts
@@ -1,16 +1,16 @@
 import { IsomorphicEvent } from './isomorphic-event.js'
 
-interface Detail<T = unknown> {
+interface Detail<T = any> {
   name: string
-  old: T | null
-  value: T | null
+  old?: T
+  value?: T
 }
 
 /**
  * @category Event
  */
-export class StateChangeEvent<T = unknown> extends IsomorphicEvent<Detail<T>> {
-  constructor(name: string, old: T | null, value: T | null) {
+export class StateChangeEvent<T = any> extends IsomorphicEvent<Detail<T>> {
+  constructor(name: string, old: T | undefined, value: T | undefined) {
     super('state-change', { detail: { name, old, value } })
   }
 }
