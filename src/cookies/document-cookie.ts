b3ac72513fea192feecf0baf47034045472651a6
Dario Sechi <dario.sechi.rnd@gmail.com>
2025-09-27 14:06:11 +0200

    Update document cookie to use new sync cookie class

--- src/cookies/document-cookie.ts
+++ src/cookies/document-cookie.ts
@@ -1,24 +1,104 @@
-import { Cookie, isDocumentNotDefined } from '@aracna/core'
+import { CookieItem, CookieObject, SyncCookie, deserializeCookie, isDocumentNotDefined, serializeCookie } from '@aracna/core'
 
 /**
  * The `DocumentCookie` class is a `Cookie` that uses the `document.cookie` API.
  *
  * [Aracna Reference](https://aracna.dariosechi.it/web/cookies/document-cookie)
  */
-export const DocumentCookie: Cookie = new Cookie(
+export const DocumentCookie: SyncCookie = new SyncCookie(
   'DocumentCookie',
   () => {
+    let object: CookieObject | Error
+
     if (isDocumentNotDefined()) {
-      return ''
+      throw new Error('Document is not defined.')
     }
 
-    return document.cookie
+    object = deserializeCookie(document.cookie)
+    if (object instanceof Error) throw object
+
+    for (let k in object) {
+      let cookie: string | Error
+
+      cookie = serializeCookie(k, '', { expires: new Date(0) })
+      if (cookie instanceof Error) throw cookie
+
+      document.cookie = cookie
+    }
   },
-  (cookie: string) => {
+  <T extends CookieItem>(key: string) => {
+    let item: T, object: CookieObject | Error
+
+    // @ts-expect-error
+    item = {}
+
     if (isDocumentNotDefined()) {
-      return
+      throw new Error('Document is not defined.')
     }
 
-    document.cookie = cookie
+    object = deserializeCookie(document.cookie)
+    if (object instanceof Error) throw object
+
+    for (let [k, v] of Object.entries(object)) {
+      if (k.startsWith(key + DocumentCookie.getSeparator())) {
+        // @ts-expect-error
+        item[k.replace(key + DocumentCookie.getSeparator(), '')] = v
+      }
+    }
+
+    return item
+  },
+  (key: string) => {
+    let object: CookieObject | Error
+
+    if (isDocumentNotDefined()) {
+      throw new Error('Document is not defined.')
+    }
+
+    object = deserializeCookie(document.cookie)
+    if (object instanceof Error) throw object
+
+    for (let k in object) {
+      if (k.startsWith(key + DocumentCookie.getSeparator())) {
+        return true
+      }
+    }
+
+    return false
+  },
+  (key: string) => {
+    let object: CookieObject | Error
+
+    if (isDocumentNotDefined()) {
+      throw new Error('Document is not defined.')
+    }
+
+    object = deserializeCookie(document.cookie)
+    if (object instanceof Error) throw object
+
+    for (let k in object) {
+      if (k.startsWith(key + DocumentCookie.getSeparator())) {
+        let cookie: string | Error
+
+        cookie = serializeCookie(k, '', { expires: new Date(0) })
+        if (cookie instanceof Error) throw cookie
+
+        document.cookie = cookie
+      }
+    }
+  },
+  <T extends CookieItem>(key: string, item: T) => {
+    if (isDocumentNotDefined()) {
+      throw new Error('Document is not defined.')
+    }
+
+    for (let [k, v] of Object.entries(item)) {
+      let cookie: string | Error
+
+      cookie = serializeCookie(key + DocumentCookie.getSeparator() + k, v)
+      if (cookie instanceof Error) throw cookie
+
+      document.cookie = cookie
+    }
   }
 )
--- tests/cookies/document-cookie.test.ts
+++ tests/cookies/document-cookie.test.ts
@@ -1,21 +1,49 @@
 import { beforeEach, describe, expect, it } from 'vitest'
-import { DocumentCookie } from '../../src'
+import { DocumentCookie } from '../../src/cookies/document-cookie'
 
 describe('DocumentCookie', () => {
   beforeEach(() => {
     DocumentCookie.clear()
   })
 
-  it('sets and gets', () => {
+  it('clears', () => {
+    DocumentCookie.set('person', { name: 'john' })
+    expect(DocumentCookie.has('person')).toBeTruthy()
+    DocumentCookie.clear()
+    expect(DocumentCookie.has('person')).toBeFalsy()
+  })
+
+  it('gets', () => {
+    expect(DocumentCookie.get('person')).toStrictEqual({})
     DocumentCookie.set('person', { name: 'john' })
     expect(DocumentCookie.get('person')).toStrictEqual({ name: 'john' })
+  })
+
+  it('has', () => {
+    DocumentCookie.set('person', { name: 'john' })
+    expect(DocumentCookie.has('person')).toBeTruthy()
+  })
+
+  it('removes', () => {
+    DocumentCookie.set('person', { name: 'john' })
+    expect(DocumentCookie.has('person')).toBeTruthy()
+    DocumentCookie.remove('person')
+    expect(DocumentCookie.has('person')).toBeFalsy()
+  })
+
+  it('sets', () => {
+    DocumentCookie.set('person', { name: 'john' })
+    expect(DocumentCookie.has('person')).toBeTruthy()
   })
 
   it('does not throw if document is undefined', () => {
     // @ts-ignore
     delete global.document
 
-    expect(DocumentCookie.set('person', { name: 'john' })).toBeUndefined()
-    expect(DocumentCookie.get('person')).toStrictEqual({})
+    expect(DocumentCookie.clear()).toBeInstanceOf(Error)
+    expect(DocumentCookie.get('person')).toBeInstanceOf(Error)
+    expect(DocumentCookie.has('person')).toBeFalsy()
+    expect(DocumentCookie.remove('person')).toBeInstanceOf(Error)
+    expect(DocumentCookie.set('person', {})).toBeInstanceOf(Error)
   })
 })
